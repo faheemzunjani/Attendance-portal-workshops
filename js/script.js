cabcdae5eeca3a3bb4f21e515097c393bd9e01a5
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-08-13 11:07:17 +0530

    redesigned functionality for the page

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.getElementById('input-boxes').addEventListener('keydown', function(even
 		var newInput = document.createElement('input');
 		newInput.setAttribute('class', 'input-field col s2 l2 m2');
 		newInput.setAttribute('maxlength', '10');
+		newInput.setAttribute('placeholder', 'IIIT*******');
 		this.appendChild(newInput);
 		newInput.focus();
 	}
@@ -21,3 +22,25 @@ document.getElementById('input-boxes').addEventListener('keydown', function(even
 		newInput.focus();
 	} 
 });
+
+
+function submitForm() {
+ // add other input elements from #inpub-boxes
+ // create a url 
+	//  var http = new XMLHttpRequest();
+	// var url = "get_data.php";
+	// var params = "lorem=ipsum&name=binny";
+	// http.open("POST", url, true);
+
+	// //Send the proper header information along with the request
+	// http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	// http.onreadystatechange = function() {//Call a function when the state changes.
+	//     if(http.readyState == 4 && http.status == 200) {
+	//         alert(http.responseText);
+	//     }
+	// }
+	// http.send(params);
+	console.log("Form submitted");
+
+}
