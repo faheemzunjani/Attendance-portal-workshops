e046834d92fbb9e235cbcb45f8c6f6924c7249e1
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-08-14 13:36:49 +0530

    frontend ready

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
+var student_count = 1;
 document.getElementById('input-boxes').addEventListener('keydown', function(event) {
 	if (event.keyCode === 13) {
+		if (event.target.nodeName != "INPUT") {
+			return false;
+		}
+		if (event.target.nodeName == 'BUTTON') {
+			event.preventDefault();
+			return false;
+		}
 		var newInput = document.createElement('input');
 		newInput.setAttribute('class', 'input-field col s2 l2 m2');
 		newInput.setAttribute('maxlength', '10');
@@ -21,6 +29,45 @@ document.getElementById('input-boxes').addEventListener('keydown', function(even
 		this.appendChild(newInput);
 		newInput.focus();
 	} 
+	
+	// rename input elements' name
+	var students = this.getElementsByTagName('input');
+	for (var i = 0; i < students.length; i++) {
+		students[i].setAttribute('name', 'student['+i+']');
+	}
+	student_count = students.length;
+});
+
+document.getElementsByTagName('button')[0].addEventListener('click', function() {
+	var myform = document.forms[0];
+	var username = myform.username.value;
+	var password = myform.password.value;
+	var wing = myform.wing.value;
+	if (username.length < 10 || password.length == 0) {
+		alert("Please fill all fields properly");
+		return false;
+	}
+	if (wing <= 0) {
+		alert("Please select a wing from the list.");
+		return false;
+	}
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function(){
+		if(xmlhttp.readyState==4 && xmlhttp.status==200){
+			if (xmlhttp.responseText == '200') {
+				document.forms[0].submit();
+			} else if(xmlhttp.responseText == '401'){
+				alert("You are not authorized to take attendance. :(");
+			} else if(xmlhttp.responseText == '403') {
+				alert("Invalid id/password");
+			} else {
+				alert("Cannot process at the moment. Try again or take some alternatives.");
+			}
+		}
+	}
+	xmlhttp.open("GET","logincheck.php?roll="+username+"&pass="+password,true);
+	xmlhttp.send();
 });
 
 
@@ -41,6 +88,6 @@ function submitForm() {
 	//     }
 	// }
 	// http.send(params);
-	console.log("Form submitted");
+	document.forms[0].submit();
 
 }
